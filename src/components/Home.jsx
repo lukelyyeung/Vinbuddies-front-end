8695c32db39eeadcf96ca0fd232ca0982f96376b
Lok Yiu <lukeyeung@m800.com>
2018-11-01 01:14:33 +0800

    Add bem-cn and refactor the homePage.scss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Jumbotron, Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
+import bem from "../utils/bem";
+
+const block = bem("home");
+const hero = block("hero");
 export const Home = () => (
-    <div>
-        <Jumbotron className="flexBox-row">
-            <div className="bg"/>
-            <div className="overlay"/>
-            <div className="flexBox-column">
-                <h4 className="text-center">Embrace the WineTech future</h4>
-                <h2>VinBuddies</h2>
-                <Link to="/login">
-                    <Button className="hero-button">GET STARTED</Button>
-                </Link>
-            </div>
-        </Jumbotron>
-    </div>
+  <section className={block()}>
+    <Jumbotron className={hero.mix("flexBox-row")()}>
+      <div className={hero("background")()} />
+      <div className={hero("overlay")()} />
+      <div className="flexBox-column">
+        <h4 className="text-center">Embrace the WineTech future</h4>
+        <h2>VinBuddies</h2>
+        <Link to="/login">
+          <Button className={hero("button")()}>GET STARTED</Button>
+        </Link>
+      </div>
+    </Jumbotron>
+  </section>
 );
---
+++ src/utils/bem.js
@@ -0,0 +1,8 @@
+import bem from "bem-cn";
+
+bem.setup({
+  el: "__",
+  mod: "--"
+});
+
+export default bem;
